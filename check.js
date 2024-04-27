a7847f2a7b0f5bd0a02ec40d47fb21a8fb5ad2f8
devvivekpatel <developerpatel.vivek@gmail.com>
2024-04-27 12:13:17 +0530

    questions

--- check.js
+++ check.js
@@ -1,11 +1,57 @@
-let a =6;
-let b = 6;
-let c = a*b;
-console.log(a);
-console.log(b);
-console.log(c);
+// let a =6;
+// let b = 6;
+// let c = a*b;
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
-function sum(a,b,c){
-    return a+b+c;
+// function sum(a,b,c){
+//     return a+b+c;
+// }
+// console.log(sum(a,b,c));
+
+
+
+// function calculation(greet){
+//     let a = "vivek patel";
+
+//     setInterval(function(){
+//         greet(a)
+//     },1000)
+
+// }
+
+// function greet(result){
+
+//     // let text = document.getElementById("h");
+//     // text.innerHTML = `<BR>HELLO ${result} 🤐🤐`;
+//         console.log(`Hello ${result}`)
+//     // document.write("<BR>HELLO : "+result," 🤐")
+// }
+// calculation(greet);
+
+var count =0;
+
+function greet(){
+    if(count>=5){
+        count =0;
+    }
+    count++;
+
+    if(count%2 != 0 && count%5 !=0){
+        console.log("Tick");
+       
+    }
+    else if(count%2==0){
+        console.log("Tock");
+    
+    }
+    else if(count%5 ==0){
+        console.log("Tick Tock");
+       
+    }
+    
 }
-console.log(sum(a,b,c));
+setInterval(greet,1000);
+
+    
